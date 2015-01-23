8e9eb585488a1a8487bd2ef65903a99f3d54346f
Nick Sutterer <apotonick@gmail.com>
2015-01-23 15:56:54 +1100

    cleanup for chapter 5.

--- app/concepts/comment/crud.rb
+++ app/concepts/comment/crud.rb
@@ -17,8 +17,6 @@ class Comment < ActiveRecord::Base
       end
 
 
-      # property :endorsement, virtual: true #, type: Virtus::Attribute::Boolean
-
       property :body
       property :weight
       property :thing
@@ -36,8 +34,6 @@ class Comment < ActiveRecord::Base
     def process(params)
       validate(params[:comment]) do |f|
         f.save # save comment and user.
-
-        # Endorsement.create(user: f.model.user, thing: f.model.thing) if f.endorsement == "1"
       end
     end
 
