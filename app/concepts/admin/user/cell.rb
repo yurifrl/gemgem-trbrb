b95c7457a50670e893f21c1a7dff1ec81c0d530f
Nick Sutterer <apotonick@gmail.com>
2015-07-05 21:21:51 +1000

    cleanup.

--- app/concepts/admin/user/cell.rb
+++ app/concepts/admin/user/cell.rb
@@ -3,31 +3,16 @@ require_dependency "user/cell"
 module Admin
   module User
     class Cell < ::User::Cell
-
-
       def right
         render
       end
 
-      # def authorships
-      #   return unless model.authorships.size > 0
-
-      #   "(#{model.authorships.count}) " +
-      #    model.authorships.map { |as| link_to as.thing.name, thing_path(as.thing) }.join(", ")
-      # end
-
       def thing_link(authorship)
-        # return unless authorship.confirmed == 1
         link_to authorship.thing.name, thing_path(authorship.thing), class: authorship.confirmed=="1" ? "confirmed" : "unconfirmed"
       end
 
-
-      def confirmed?
-        puts "@@@@@ #{Session::Authenticatable.new(model).confirmed?.inspect}"
-        Session::Authenticatable.new(model).confirmed?
-      end
       def checked
-        return "checked=1" if confirmed?
+        "checked=1" if Session::Authenticatable.new(model).confirmed?
       end
     end # Cell
   end
