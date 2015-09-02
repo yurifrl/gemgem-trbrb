2c6211ee85ce0655e4b846f36b500c12f3e50a5c
Nick Sutterer <apotonick@gmail.com>
2015-09-02 11:24:57 +1000

    extend thing/delete test.

--- test/concepts/thing/delete_test.rb
+++ test/concepts/thing/delete_test.rb
@@ -1,45 +1,81 @@
 require "test_helper"
 
 class ThingDeleteTest < MiniTest::Spec
-  it "authorless can't be deleted" do
-    thing = Thing::Create.(thing: {name: "Rails"}).model
-
-    assert_raises Pundit::NotAuthorizedError do
-      Thing::Delete::SignedIn.(id: thing.id)
-    end
-    thing.destroyed?.must_equal false
-  end
-
-  # signed in.
-
   let (:current_user) { User::Create.(user: {email: "fred@trb.org"}).model }
+  let (:admin)        { User::Create.(user: {email: "admin@trb.org"}).model }
 
-  it "can't be deleted because we're not author" do
-    thing = Thing::Create::SignedIn.(thing: {name: "Rails", users: [{"email"=>"joe@trb.org"}]}).model
 
-    assert_raises Pundit::NotAuthorizedError do
-      thing = Thing::Delete::SignedIn.(id: thing.id, current_user: current_user).model
+  describe "authorless" do
+    let (:thing) { Thing::Create.(thing: {name: "Rails"}).model }
+
+    # anonymous
+    it "can't be deleted" do
+      assert_raises Pundit::NotAuthorizedError do
+        Thing::Delete.(id: thing.id)
+      end
     end
-    thing.destroyed?.must_equal false
+
+    # signed in.
+    it "can't be deleted" do
+      assert_raises Pundit::NotAuthorizedError do
+        Thing::Delete.(id: thing.id, current_user: current_user)
+      end
+    end
+
+    # admin
+    it "can be deleted as admin" do
+      deleted = Thing::Delete.(id: thing.id, current_user: admin).model
+      deleted.destroyed?.must_equal true
+    end
   end
 
-  it "deleted by author, no image, no comments" do
-    thing = Thing::Create::SignedIn.(thing: {name: "Rails", is_author: "1"}, current_user: current_user).model
-    thing = Thing::Delete::SignedIn.(id: thing.id, current_user: current_user).model
-    thing.destroyed?.must_equal true
-  end
 
-  it "deleted by author, with images and comments" do
-    thing = Thing::Create::SignedIn.(thing: {name: "Rails", is_author: "1", file: File.open("test/images/cells.jpg")}, current_user: current_user).model
+  describe "with authors" do
+    let (:thing) { Thing::Create::SignedIn.(thing: {name: "Rails", users: [{"email"=>"joe@trb.org"}]}).model }
 
-    file = Thing::Cell::Decorator.new(thing)
+    # anonymous
+    it "can't be deleted" do
+      assert_raises Pundit::NotAuthorizedError do
+        Thing::Delete.(id: thing.id)
+      end
+    end
 
-    thing = Thing::Delete::SignedIn.(id: thing.id, current_user: current_user).model
-    thing.destroyed?.must_equal true
+    # signed in, not author
+    it "can't be deleted because we're not author" do
+      thing = Thing::Create.(thing: {name: "Rails", users: [{"email"=>"joe@trb.org"}]}, current_user: current_user).model
 
-    # image must be deleted, too.
-    file = Thing::Cell::Decorator.new(thing)
-    File.exists?("public#{file.image[:thumb].url}").must_equal false
-    File.exists?("public#{file.image[:original].url}").must_equal false
+      assert_raises Pundit::NotAuthorizedError do
+        Thing::Delete.(id: thing.id, current_user: current_user)
+      end
+    end
+
+    # signed in is author
+    it "deleted by author, no image, no comments" do
+      thing = Thing::Create.(thing: {name: "Rails", is_author: "1"}, current_user: current_user).model
+      thing = Thing::Delete.(id: thing.id, current_user: current_user).model
+      thing.destroyed?.must_equal true
+    end
+
+    # admin
+    it "deleted by admin" do
+      thing = Thing::Create.(thing: {name: "Rails", is_author: "1"}, current_user: current_user).model
+      thing = Thing::Delete.(id: thing.id, current_user: admin).model
+      thing.destroyed?.must_equal true
+    end
+
+
+    it "deleted by author, with images and comments" do
+      thing = Thing::Create.(thing: {name: "Rails", is_author: "1", file: File.open("test/images/cells.jpg")}, current_user: current_user).model
+
+      file = Thing::Cell::Decorator.new(thing)
+
+      thing = Thing::Delete.(id: thing.id, current_user: current_user).model
+      thing.destroyed?.must_equal true
+
+      # image must be deleted, too.
+      file = Thing::Cell::Decorator.new(thing)
+      File.exists?("public#{file.image[:thumb].url}").must_equal false
+      File.exists?("public#{file.image[:original].url}").must_equal false
+    end
   end
 end
