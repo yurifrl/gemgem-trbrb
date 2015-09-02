f8cd981c7a3cb48612a73780fb3b0d5e6a616a7d
Nick Sutterer <apotonick@gmail.com>
2015-09-02 15:55:24 +1000

    implement Resolver.

--- app/concepts/thing/delete.rb
+++ app/concepts/thing/delete.rb
@@ -1,14 +1,33 @@
 class Thing::Delete < Trailblazer::Operation
-  include CRUD::ClassBuilder
-  model Thing, :find
 
-  include Trailblazer::Operation::Policy::Pundit
+  module Resolver
+    def self.included(includer)
+      includer.class_eval do
+        include Trailblazer::Operation::Policy::Pundit # ::build_policy
+        include Trailblazer::Operation::CRUD::ClassBuilder # ::build_operation
+
+        extend BuildOperation
+      end
+    end
+
+    module BuildOperation
+      def build_operation(params, options={})
+        model  = model!(*params)
+        policy = build_policy(model, *params)
+
+        build_operation_class(model, policy, *params).new(model, options)
+        # super([model, params], [model, options]) # calls: builds ->(model, params), and Op.new(model, params)
+      end
+    end
+  end
+
+
+  include Thing::Delete::Resolver # CRUD::ClassBuilder, Policy
+  model Thing, :find
   policy Thing::Policy, :delete?
 
   # self.builder_class = Create.builder_class
-  builds -> (model, params) do
-    policy = build_policy(model, params)
-
+  builds -> (model, policy, params) do
     return self::SignedIn if policy.admin?
     return self::SignedIn if policy.signed_in?
   end
